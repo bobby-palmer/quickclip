8334b7a34da7e3e2a82cf81b15db5c7f0ee329d2
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-06-30 11:01:26 -0400

    added an Index structure to keep track off all book marks and read/write to file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{Parser, Subcommand};
+use serde_derive::{Deserialize, Serialize};
+use std::{collections::BTreeMap, env, fs, path::PathBuf, str::FromStr};
+use toml;
 
 #[derive(Subcommand)]
 enum Commands {
@@ -12,11 +15,41 @@ struct Cli {
     #[clap(subcommand)]
     command: Option<Commands>,
 }
-fn init() {}
+
+#[derive(Deserialize, Serialize)]
+struct Index(BTreeMap<String, PathBuf>);
+impl Index {
+    fn from_file(file: PathBuf) -> Self {
+        let content = fs::read_to_string(file).unwrap();
+        toml::from_str(&content).unwrap()
+    }
+    fn add(&mut self, alias: String) {
+        self.0
+            .insert(alias, env::current_dir().expect("error getting directory"));
+    }
+    fn remove(&mut self, alias: String) {
+        self.0.remove(&alias);
+    }
+    fn list(&self) {
+        self.0.iter().for_each(|(key, val)| {
+            println!("{} in place of : {}", key, val.to_str().unwrap());
+        })
+    }
+    fn to_file(&self, file: PathBuf) {
+        let index = toml::to_string_pretty(self).unwrap();
+        fs::write(file, index).unwrap();
+    }
+}
 
 fn list() {}
 
-fn mark(alias: Option<String>) {}
+fn mark(alias: Option<String>) {
+    let filepath = PathBuf::from_str("/Users/bobbypalmer/Desktop/marks.toml");
+    let out = PathBuf::from_str("/Users/bobbypalmer/Desktop/out.toml");
+    let mut index = Index::from_file(filepath.unwrap());
+    index.add(String::from("Test"));
+    index.to_file(out.unwrap());
+}
 
 fn main() {
     let args = Cli::parse();
