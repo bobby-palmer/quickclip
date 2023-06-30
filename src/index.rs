cbe50ba15823519d288cd5ae3335b71987191eec
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-06-30 13:32:36 -0400

    improved the read and write from file to use a reference instead of ownership

--- src/index.rs
+++ src/index.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 #[derive(Deserialize, Serialize)]
 pub struct Index(BTreeMap<String, PathBuf>);
 impl Index {
-    pub fn from_file(file: PathBuf) -> Self {
+    pub fn from_file(file: &PathBuf) -> Self {
         let content = fs::read_to_string(file).unwrap();
         toml::from_str(&content).unwrap()
     }
@@ -23,7 +23,7 @@ impl Index {
             println!("{} in place of : {}", key, val.to_str().unwrap());
         })
     }
-    pub fn to_file(&self, file: PathBuf) {
+    pub fn to_file(&self, file: &PathBuf) {
         let index = toml::to_string_pretty(self).unwrap();
         fs::write(file, index).unwrap();
     }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use mark::defaults;
 use mark::index::Index;
 use mark::input::{Cli, Commands};
 use std::{path::PathBuf, str::FromStr};
@@ -6,11 +7,10 @@ use std::{path::PathBuf, str::FromStr};
 fn list() {}
 
 fn mark(alias: Option<String>) {
-    let filepath = PathBuf::from_str("/Users/bobbypalmer/Desktop/marks.toml");
-    let out = PathBuf::from_str("/Users/bobbypalmer/Desktop/out.toml");
-    let mut index = Index::from_file(filepath.unwrap());
+    let filepath = defaults::get_config().unwrap();
+    let mut index = Index::from_file(&filepath);
     index.add(String::from("Test"));
-    index.to_file(out.unwrap());
+    index.to_file(&filepath);
 }
 
 fn main() {
