c100f47dd023e7c461fa39d8241d80a97b749184
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-07-16 14:24:37 -0400

    added an initializer for the config

--- src/init.rs
+++ src/init.rs
@@ -1,5 +1,16 @@
+use std::fs::File;
+
+use crate::defaults;
+
 // used to create new config file and eventually will be able to create the goto function for most
 // major shells
 pub fn init() -> bool {
-    unimplemented!();
+    if defaults::get_config().is_ok(){
+        false
+    }else {
+        // no current config
+        let file = dirs::config_dir().unwrap().join("mark/config.toml");
+        let _ = File::create(file);
+        true
+    }
 }
