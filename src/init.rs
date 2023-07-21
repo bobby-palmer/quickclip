6fd2cab2f4a5cda6ad8afceaa1bc1a5a045c1718
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-07-21 15:01:02 -0400

    switch to .txt format

--- src/init.rs
+++ src/init.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 
 use crate::defaults;
 
-const FISH: &'static str = include_str!("./resources/scripts/init.fish");
+const FISH: &'static str = include_str!("./resources/scripts/fish.txt");
 
 // used to create new config file and eventually will be able to create the goto function for most
 // major shells
